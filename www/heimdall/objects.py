f7914d4c70f89a3bb01ea4d4537220df14dd6170
svandecappelle <steeve.vandecappelle@gmail.com>
2013-06-28 17:20:29 +0200

    adding demande access system

---
+++ www/heimdall/objects.py
@@ -0,0 +1,7 @@
+class Statistics:
+	
+	def __init__(self, user_count,server_count,permissions_count, demands_count):
+		self.user_count = user_count
+		self.server_count = server_count
+		self.permissions_count = permissions_count
+		self.demands_count = demands_count
