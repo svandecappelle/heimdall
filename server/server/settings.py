c41d87a243d4cf32080701d339b0fbc6dc40ee42
svandecappelle <steeve.vandecappelle@gmail.com>
2013-09-26 15:45:23 +0200

    settings on static files

--- server/server/settings.py
+++ server/server/settings.py
@@ -65,7 +65,8 @@ MEDIA_URL = '/medias/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(ABSOLUTE_PATH, 'collected_static')
+STATIC_ROOT = "/home/heimdall/static/"
+#STATIC_ROOT = os.path.join(ABSOLUTE_PATH, 'collected_static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -76,7 +77,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    STATIC_ROOT,
 )
 
 # List of finder classes that know how to find static files in
--- server/server/urls.py
+++ server/server/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
+
 admin.autodiscover()
+
 
 handler404 = 'heimdall.utils.handler404'
 handler500 = 'heimdall.utils.handler500'
