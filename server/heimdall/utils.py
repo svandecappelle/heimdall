3e45c7916b1ba70e94b802ca79d001e9b22e28d8
mizore <me@mizore.fr>
2014-01-09 17:40:37 +0100

    Dev

--- server/heimdall/utils.py
+++ server/heimdall/utils.py
@@ -90,6 +90,7 @@ def getAvailableUsersInHost(host):
 		client.load_system_host_keys()
 		client.set_missing_host_key_policy(AutoAddPolicy())
 		if Permission.objects.get(server=host).exists():
+			print("try connect with: " + str(Permission.objects.get(server=host)[:1]) + " on: " + host.hostname)
 			client.connect('%s' % host.hostname, port=host.port, username=Permission.objects.get(server=host)[:1])
 
 			# Check user allowed to replicator
