7abf6a3c276a5ad437f62890b5e0ce09167a6a03
mizore <me@mizore.fr>
2014-01-16 16:04:28 +0100

    Error uindex on permission user for refreshing user list availabel

--- server/heimdall/utils.py
+++ server/heimdall/utils.py
@@ -90,8 +90,8 @@ def getAvailableUsersInHost(host):
 		client.load_system_host_keys()
 		client.set_missing_host_key_policy(AutoAddPolicy())
 		if Permission.objects.filter(server=host).exists():
-			print("try connect with: " + str(Permission.objects.filter(server=host)[1].hostuser) + " on: " + host.hostname)
-			client.connect('%s' % host.hostname, port=host.port, username=Permission.objects.filter(server=host)[1].hostuser)
+			print("try connect with: " + str(Permission.objects.filter(server=host)[0].hostuser) + " on: " + host.hostname)
+			client.connect('%s' % host.hostname, port=host.port, username=Permission.objects.filter(server=host)[0].hostuser)
 
 			# Check user allowed to replicator
 			stdin, stdout, stderr = client.exec_command("cut -d':' -f1 /etc/passwd | grep --invert-match -E '%s'" % matches)
