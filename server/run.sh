014f8408dea36c12847717af9a220001389e8cab
svandecappelle <steeve.vandecappelle@gmail.com>
2014-05-06 10:34:25 +0200

    Update run.sh to do not start nginx and rabbitmq

--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,9 @@
 # You should use it in a screen.. 
 # Starting Gunicorn
-sudo service rabbitmq-server start
-sudo service nginx start
-python manage.py run_gunicorn
+
+# only if not started before
+#sudo service rabbitmq-server start
+#sudo service nginx start
+#python manage.py run_gunicorn
+
+gunicorn server.wsgi:application --bind 127.0.0.1:8080 --log-file /var/log/www/heimdall.gunicorn.log --log-level debug
